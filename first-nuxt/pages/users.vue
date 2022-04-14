6789f54708a2d8961680f7cf48923ba194d3976a
Max <v2asault@yahoo.com.tw>
2022-04-14 22:43:45 +0800

    add nested routes

---
+++ first-nuxt/pages/users.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input type="text" v-model="userId" />
+    <button @click="gotoUser">Load User</button>
+    <nuxt-child />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: '',
+    }
+  },
+  methods: {
+    gotoUser() {
+      this.$router.push('users' + '/' + this.userId)
+    },
+  },
+}
+</script>
--- first-nuxt/pages/users/index.vue
+++ first-nuxt/pages/users/index.vue
@@ -1,23 +1,3 @@
 <template>
-  <div>
-    <input type="text" v-model="userId" />
-    <button @click="gotoUser">Load User</button>
-  </div>
+  <p>Please enter an id and click the button.</p>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      userId: '',
-    }
-  },
-  methods: {
-    gotoUser() {
-      this.$router.push('users' + '/' + this.userId)
-    },
-  },
-}
-</script>
-
-<style></style>

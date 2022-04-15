92df7e417947e99137bd841b32bbdc1e2a210133
Max <v2asault@yahoo.com.tw>
2022-04-15 23:24:47 +0800

    1. project init 2. add landing page 3. add post page 4. add post component

---
+++ my-project/pages/about/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about-page">
+    <h1>About Me</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae iusto
+      eius et, amet sed rem saepe est ut eligendi! Reprehenderit sapiente
+      placeat harum, excepturi nobis aliquam possimus similique autem maiores!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae iusto
+      eius et, amet sed rem saepe est ut eligendi! Reprehenderit sapiente
+      placeat harum, excepturi nobis aliquam possimus similique autem maiores!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae iusto
+      eius et, amet sed rem saepe est ut eligendi! Reprehenderit sapiente
+      placeat harum, excepturi nobis aliquam possimus similique autem maiores!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae iusto
+      eius et, amet sed rem saepe est ut eligendi! Reprehenderit sapiente
+      placeat harum, excepturi nobis aliquam possimus similique autem maiores!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.about-page {
+  padding: 30px;
+  text-align: center;
+}
+</style>

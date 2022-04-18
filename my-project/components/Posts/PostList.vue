f9c626137c9ef4eb3b61171d7238c5512c141352
Max <v2asault@yahoo.com.tw>
2022-04-18 23:47:24 +0800

    add create post feature in admin page

--- my-project/components/Posts/PostList.vue
+++ my-project/components/Posts/PostList.vue
@@ -2,12 +2,14 @@
   <section class="post-list">
     <PostPreview
       id="1"
+      :isAdmin="isAdmin"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
       title="title text 1"
       previewText="preview text 1"
     />
     <PostPreview
       id="2"
+      :isAdmin="isAdmin"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
       title="title text 2"
       previewText="preview text 2"
@@ -20,6 +22,12 @@ import PostPreview from '@/components/Posts/PostPreview.vue'
 export default {
   components: {
     PostPreview,
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
 }
 </script>

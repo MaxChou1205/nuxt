c3989b44382ad36804aa636a5be7b0705203f9fc
Max <v2asault@yahoo.com.tw>
2022-09-08 21:06:38 +0800

    init

--- dark-mode/app.vue
+++ dark-mode/app.vue
@@ -1,15 +1,19 @@
-<template>
-  <div>
-    <Nav />
-    <Heading />
-    <Cards />
-  </div>
-</template>
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  font-family: Georgia;
-}
-</style>
+<script setup lang="ts">
+  const { isDarkMode } = useDarkMode();
+  </script>
+  
+  <template>
+    <div :style="isDarkMode ? { backgroundColor: 'black' } : null">
+      <Nav />
+      <Heading />
+      <Cards />
+    </div>
+  </template>
+  
+  <style>
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: Georgia;
+  }
+  </style>

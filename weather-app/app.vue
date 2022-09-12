b22f8682e6be78890bbb5fdad2d68cc63153fa48
Max <v2asault@yahoo.com.tw>
2022-09-12 23:11:41 +0800

    add cookie composable

--- weather-app/app.vue
+++ weather-app/app.vue
@@ -1,5 +1,8 @@
 <script setup>
-const search = ref("tainan");
+const cookie = useCookie('city');
+if (!cookie.value) cookie.value = "Toronto";
+
+const search = ref(cookie.value);
 const inputField = ref("");
 const background = ref("");
 
@@ -9,28 +12,34 @@ const background = ref("");
 
 const {data: city, error, refresh} = useAsyncData('city', async () => {
   let response;
-  response = await $fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}`, {
-    params: {
-      units: "metric",
-      appid: "2f84605ce3a84ad78ae4a5a9c34bcfde",
-    },
-  })
 
-  const temp = response.main.temp;
-  if (temp <= -10) {
-    background.value =
-        "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
-  } else if (temp > -10 && temp <= 0) {
-    background.value =
-        "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
-  } else if (temp > 0 && temp <= 10) {
-    background.value =
-        "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
-  } else {
-    background.value =
-        "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+  try {
+    response = await $fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}`, {
+      params: {
+        units: "metric",
+        appid: "2f84605ce3a84ad78ae4a5a9c34bcfde",
+      },
+    })
+
+    cookie.value = search.value;
+
+    const temp = response.main.temp;
+    if (temp <= -10) {
+      background.value =
+          "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+    } else if (temp > -10 && temp <= 0) {
+      background.value =
+          "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+    } else if (temp > 0 && temp <= 10) {
+      background.value =
+          "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
+    } else {
+      background.value =
+          "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+    }
+  } catch (e) {
+
   }
-
   return response;
 }, {
   watch: [search]
@@ -68,6 +77,7 @@ const handleClick = () => {
             class="w-1/2 h-10"
             placeholder="Search a city..."
             v-model="inputField"
+            @keyup.enter="handleClick"
         />
         <button class="bg-sky-400 w-20 text-white h-10" @click="handleClick">
           Search

613895775f8ec6d727d5ca3b51aa6ff2805b56ae
Max <v2asault@yahoo.com.tw>
2022-09-12 22:51:26 +0800

    fetch data with http request (usefetch and useAsyncData)

--- weather-app/app.vue
+++ weather-app/app.vue
@@ -1,17 +1,64 @@
+<script setup>
+const search = ref("tainan");
+const inputField = ref("");
+const background = ref("");
+
+// const {data: city, error} = useFetch(
+//     () => `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=2f84605ce3a84ad78ae4a5a9c34bcfde`
+// );
+
+const {data: city, error, refresh} = useAsyncData('city', async () => {
+  let response;
+  response = await $fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}`, {
+    params: {
+      units: "metric",
+      appid: "2f84605ce3a84ad78ae4a5a9c34bcfde",
+    },
+  })
+
+  const temp = response.main.temp;
+  if (temp <= -10) {
+    background.value =
+        "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+  } else if (temp > -10 && temp <= 0) {
+    background.value =
+        "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+  } else if (temp > 0 && temp <= 10) {
+    background.value =
+        "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
+  } else {
+    background.value =
+        "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+  }
+
+  return response;
+}, {
+  watch: [search]
+})
+
+const handleClick = () => {
+  const formatedSearch = inputField.value.trim().split(" ").join("+");
+  search.value = formatedSearch;
+  inputField.value = "";
+  // refresh();
+};
+</script>
+
 <template>
   <div class="h-screen relative overflow-hidden">
-    <img/>
+    <img :src="background"/>
     <div class="absolute w-full h-full top-0 overlay"/>
     <div class="absolute w-full h-full top-0 p-48">
       <div class="flex justify-between">
         <div>
-          <h1 class="text-7xl text-white">City</h1>
+          <h1 class="text-7xl text-white">{{ city.name }}</h1>
           <p class="font-extralight text-2xl mt-2 text-white">Sunday Dec 9th</p>
-          <img/>
+          <img :src="`https://openweathermap.org/img/wn/${city.weather[0].icon}@4x.png`"
+               class="w-56 icon"/>
         </div>
         <div>
           <p class="text-9xl text-white font-extralight">
-            20°
+            {{ city.main.temp }}°
           </p>
         </div>
       </div>
@@ -20,8 +67,9 @@
             type="text"
             class="w-1/2 h-10"
             placeholder="Search a city..."
+            v-model="inputField"
         />
-        <button class="bg-sky-400 w-20 text-white h-10">
+        <button class="bg-sky-400 w-20 text-white h-10" @click="handleClick">
           Search
         </button>
       </div>

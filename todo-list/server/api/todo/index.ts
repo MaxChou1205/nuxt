2f6851a687e71f63ed240b2b145d415849d4d618
Max <v2asault@yahoo.com.tw>
2022-09-13 22:56:01 +0800

    post request and body

--- todo-list/server/api/todo/index.ts
+++ todo-list/server/api/todo/index.ts
@@ -1,8 +1,23 @@
 import {db} from "../../db"
+import {v4 as uuid} from "uuid";
 
-export default defineEventHandler((e) => {
+export default defineEventHandler(async (e) => {
     const method = e.req.method
-    if (method === 'GET') {
+    if (method === "GET") {
         return db.todos;
+    } else if (method === "POST") {
+        const body = await useBody(e);
+
+        if (!body.item) throw new Error("key 'item' is required");
+
+        const newTodo = {
+            id: uuid(),
+            item: body.item,
+            completed: false
+        }
+
+        db.todos.push(newTodo)
+
+        return newTodo;
     }
 })
